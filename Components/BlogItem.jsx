6763e291e7f4b304152c25526f05a3b406e428e3
Ajay <ajayxa1@gmail.com>
2025-04-20 20:43:19 +0530

    import use client in BlogItem.jsx

--- Components/BlogItem.jsx
+++ Components/BlogItem.jsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React from "react";
 import { IoIosArrowRoundForward } from "react-icons/io";
 import Link from "next/link";
@@ -8,7 +10,10 @@ const BlogItem = ({ title, desc, image, category, id, authorImg, author }) => {
   //console.log("Author Image:", authorImg);
   // console.log("Blog Image:", image);
   return (
-    <div className="max-w-[330px] sm:max-w-[300px] bg-white border border-black hover:shadow=[-7px_7px_0_0_#000]">
+    <div
+      className="max-w-[330px] sm:max-w-[300px] bg-white border border-black hover:shadow-[ -7px_7px_0_0_#000]
+"
+    >
       <Link href={`/blogs/${id}`}>
         <Image
           src={image}
