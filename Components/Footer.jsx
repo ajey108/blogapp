8f53d3e3ab81736b35b14fa0aa22a7ccbfa14393
Ajay <ajayxa1@gmail.com>
2025-04-27 22:11:08 +0530

    Update Footer.jsx

--- Components/Footer.jsx
+++ Components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
     <div className="flex justify-around flex-col gap-2 sm:gap-0 sm:flex-row bg-black py-5 items-center">
       <FaBlog className="text-3xl text-white" />
       <p className="text-white text-xs sm:text-base">
-        © 2024 . All rights reserved.
+        © ajey108 2024 . All rights reserved.
       </p>
       <a href="https://ajay108portfolio.netlify.app/">
         <PiUserGearFill className="text-3xl text-white hover:text-gray-500 " />
